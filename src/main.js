71fd7f15410df38e6879da9fca429f38a2b6eaa4
leason-wan <9981103+leason-wan@user.noreply.gitee.com>
2022-05-04 08:44:26 +0800

    fix: render

--- src/main.js
+++ src/main.js
@@ -18,6 +18,25 @@ if (window.__POWERED_BY_QIANKUN__) {
 let router = null;
 let instance = null;
 
+function render(props = {}) {
+  const { container } = props;
+
+  router = new VueRouter({
+    mode: 'history',
+    base: process.env.BASE_URL,
+    routes
+  });
+
+  instance = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount(container ? container.querySelector('#app') : '#app')
+}
+
+if (!window.__POWERED_BY_QIANKUN__) {
+  render();
+}
+
 /**
  * bootstrap 只会在微应用初始化的时候调用一次，下次微应用重新进入时会直接调用 mount 钩子，不会再重复触发 bootstrap。
  * 通常我们可以在这里做一些全局变量的初始化，比如不会在 unmount 阶段被销毁的应用级别的缓存等。
@@ -30,17 +49,7 @@ let instance = null;
  * 应用每次进入都会调用 mount 方法，通常我们在这里触发应用的渲染方法
  */
 export async function mount(props) {
-  const { container, name } = props;
-  router = new VueRouter({
-    mode: 'history',
-    base: name ? name : process.env.BASE_URL,
-    routes
-  });
-  instance = new Vue({
-    router,
-    render: h => h(App)
-  }).$mount(container ? container.querySelector('#app') : '#app')
-
+  render(props);
   console.log('[dashboard app] mount')
 }
 
